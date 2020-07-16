65d73899d0430689d9b6a44273bc730ef358a824
wolfdog27 <kylenicholasanderson@gmail.com>
2020-07-15 17:25:34 -0700

    set up initial prompt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   password: "password",
-  database: "top_songsDB"
+  database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
@@ -17,3 +17,58 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 //   runSearch();
 });
+
+function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View Departments?",
+            "View Roles",
+            "View Employees",
+            "Add Departments?",
+            "Add Roles",
+            "Add Employees",
+            "Update Employee Roles",
+            "Exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View Departments?":
+          viewDepartment();
+          break;
+  
+        case "View Roles":
+          viewRoles();
+          break;
+  
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "Add Departments?":
+          addDepartments();
+          break;
+  
+        case "Add Roles":
+          addRoles();
+          break;
+  
+        case "Add Employees":
+          addEmployees();
+          break;
+  
+        case "Update Employee Roles":
+          updateEmployeeRoles();
+          break;
+  
+        case "exit":
+          connection.end();
+          console.log("Thank You For Using Employee Tracker!");
+          break;
+        }
+      });
+  }
